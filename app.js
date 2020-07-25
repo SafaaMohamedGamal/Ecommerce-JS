552aaad2e71ee11e567e8c5fde89cf280d1e6fe8
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-07-25 18:45:55 +0200

    adjust returned json data

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ class Products {
         const image = item.fields.image.fields.file.url;
         return {title, price, id, image};
       })
+      return products;
     } catch (error) {
       console.log(error);
     }
