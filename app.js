a5860828f58e78ae80e76e95fad4aa615353d08e
SafaaMohamedGamal <safaa.mohamed.gamal@gmail.com>
2020-07-25 16:50:52 +0200

    Ecommerce shopping cart with html+css+javascript

---
+++ app.js
@@ -0,0 +1,40 @@
+const cartBtn = document.querySelector('.btn-cart');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDom = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDom = document.querySelector('.products-center');
+
+//Cart to read from local storage
+let cart = [];
+
+//getting the products
+class Products {
+  async getProducts()
+  {
+    try {
+      let result = await fetch('products.json');
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+//display products
+class UI {
+}
+//local storage
+class Storage {
+}
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  const ui = new UI();
+  const products = new Products();
+  //get all products
+  products.getProducts().then(data=>console.log(data))
+})
